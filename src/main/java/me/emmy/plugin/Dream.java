dd7c9179e0cd68f4d2d3498e4523967c2d2396f6
Emmy <158222177+hmEmmy@users.noreply.github.com>
2025-08-09 19:28:21 +0200

    chore: initial project setup

--- src/main/java/me/emmy/plugin/Dream.java
+++ src/main/java/me/emmy/plugin/Dream.java
@@ -2,16 +2,14 @@ package me.emmy.plugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Dream extends JavaPlugin {
+public class Dream extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
 }
