e7070440dd6cd4045e15c131971219dc4ced6530
Emmy <158222177+hmEmmy@users.noreply.github.com>
2025-08-09 22:25:59 +0200

    chore: created classes for arenas

---
+++ src/main/java/me/emmy/plugin/feature/arena/ArenaService.java
@@ -0,0 +1,9 @@
+package me.emmy.plugin.feature.arena;
+
+/**
+ * @author Emmy
+ * @project Dream
+ * @since 09/08/2025
+ */
+public class ArenaService {
+}
---
+++ src/main/java/me/emmy/plugin/feature/arena/model/Arena.java
@@ -0,0 +1,9 @@
+package me.emmy.plugin.feature.arena.model;
+
+/**
+ * @author Emmy
+ * @project Dream
+ * @since 09/08/2025
+ */
+public class Arena {
+}
